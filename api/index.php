ea908640a893dc60a006458dc71fe23f49d1789c
jvacero <johnvictorgacero@gmail.com>
2026-08-08 02:28:09 +0800

    Mini movements ahead.

--- api/index.php
+++ api/index.php
@@ -200,7 +200,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/index.css">
+    <link rel="stylesheet" href="../assets/index.css">
 </head>
 <body>
 
